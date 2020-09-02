6152de7da96236eeca48bcdd41b60df619db7490
IlhamAtsaris <60317615+IlhamAtsaris@users.noreply.github.com>
2020-09-02 21:23:52 +0700

    Day 47 fetch API indonesia Cases

--- Day_44/data.js
+++ Day_44/data.js
@@ -14,4 +14,23 @@ fetch("https://indonesia-covid-19.mathdro.id/api/provinsi/")
   });
 
 const dt = new Date();
-document.getElementById("datetime").innerHTML = dt.toLocaleDateString();
+document.querySelector(".datetime").innerHTML = dt.toLocaleDateString();
+document.getElementById("time").innerHTML = dt.toLocaleDateString();
+
+fetch("https://api.thevirustracker.com/free-api?countryTotal=ID")
+  .then((response) => response.json())
+  .then((response) => {
+    const data = response.countrydata;
+    const total_cases = data[0].total_cases;
+    const total_sembuh = data[0].total_recovered;
+    const total_death = data[0].total_deaths;
+    const new_cases = data[0].total_new_cases_today;
+    const new_death = data[0].total_new_deaths_today;
+    const aktif = data[0].total_active_cases;
+
+    document.querySelector(".cases").innerHTML = total_cases;
+    document.querySelector(".kasus_baru").innerHTML = new_cases;
+    document.querySelector(".sembuh").innerHTML = total_sembuh;
+    document.querySelector(".meninggal").innerHTML = total_death;
+    document.querySelector(".baru").innerHTML = new_death;
+  });
